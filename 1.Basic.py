f4c8b1dbd8c15269f0d4932810e79b538ad9e7d8
Rizwan Haider <rizuu004@gmail.com>
2023-12-03 03:46:16 +0500

    Add files via upload
    First Python program

---
+++ 1.Basic.py
@@ -0,0 +1,24 @@
+name  = str(input("Enter a name       :  "))
+age   = str(input("Enter age          :  "))
+roll  = int(input("Enter Roll Number  :  "))
+
+print("Is new Student")
+print("1 yes")
+print("2 No")
+
+choice = int(input())
+
+if choice==1:
+    is_new=True
+else:
+    is_new = False
+
+if(is_new == True):
+    print("Name of new Student is        : ", name)  #it will print the name
+    print("Age of new Student is         : ", age)
+    print("Roll number of new Student is : ", roll)
+else:
+    print("Existing Student")
+    print("Name of existing Student is        : ", name)  
+    print("Age of existing Student is         : ", age)
+    print("Roll number of existing Student is : ", roll)
