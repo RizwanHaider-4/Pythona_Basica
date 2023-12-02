f00812958a547664f08eb00fc186e4465082f4fe
Rizwan Haider <rizuu004@gmail.com>
2023-12-03 03:48:50 +0500

    Update 1.Basic.py
    added a comments.

--- 1.Basic.py
+++ 1.Basic.py
@@ -14,11 +14,11 @@ else:
     is_new = False
 
 if(is_new == True):
-    print("Name of new Student is        : ", name)  #it will print the name
+    print("Name of new Student is        : ", name)  #it will print the details of new student
     print("Age of new Student is         : ", age)
     print("Roll number of new Student is : ", roll)
 else:
     print("Existing Student")
-    print("Name of existing Student is        : ", name)  
+    print("Name of existing Student is        : ", name)  # it will print the details of existing Student
     print("Age of existing Student is         : ", age)
     print("Roll number of existing Student is : ", roll)
