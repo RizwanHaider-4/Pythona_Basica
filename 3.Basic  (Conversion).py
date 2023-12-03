411af1bff39fbc54aa1b5fcf39ea1167b5b0c6b2
Rizwan Haider <rizuu004@gmail.com>
2023-12-04 01:09:48 +0500

    Add files via upload
    Thirds Basics

---
+++ 3.Basic  (Conversion).py
@@ -0,0 +1,12 @@
+birth_year = input('Enter your birth year    :  ')
+age = 2023-int(birth_year)
+print(age)
+weight = input('Enter your weight in pounds  :  ')
+print('Weight in pounds                      :  '+weight)
+kg = 2.205
+in_kg = kg*int(weight)
+print(in_kg)
+
+weight_lbs = input("Input weight in pounds      ")
+weight_kg = float(weight_lbs) * 0.545
+print(weight_kg)
