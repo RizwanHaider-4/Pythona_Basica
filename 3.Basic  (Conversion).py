d4d99d1760307e7951a4172505635087a500c62b
Rizwan Haider <rizuu004@gmail.com>
2023-12-04 01:11:29 +0500

    Update 3.Basic  (Conversion).py
    Adding comments for console screen

--- 3.Basic  (Conversion).py
+++ 3.Basic  (Conversion).py
@@ -1,11 +1,11 @@
 birth_year = input('Enter your birth year    :  ')
 age = 2023-int(birth_year)
-print(age)
+print(age)   #actual age
 weight = input('Enter your weight in pounds  :  ')
-print('Weight in pounds                      :  '+weight)
+print('Weight in pounds                      :  '+weight) # weights in pounds
 kg = 2.205
 in_kg = kg*int(weight)
-print(in_kg)
+print(in_kg)   # weight in kg
 
 weight_lbs = input("Input weight in pounds      ")
 weight_kg = float(weight_lbs) * 0.545
