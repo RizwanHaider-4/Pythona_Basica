331ea6bf1b2e0859bc16c10b01d42eb8a7bbc644
Rizwan Haider <rizuu004@gmail.com>
2023-12-05 02:35:17 +0500

    Add files via upload
    Print Function

---
+++ 5.Print.py
@@ -0,0 +1,12 @@
+# multiple values
+# sep=
+# end=
+a = 12
+b = 15
+print("hello i am very beautiful", a, b, 16) # multiple values
+print(a, b, 16, 17, sep='#') # seprate each parameters
+                              # single or double commas
+print(a, b, 16,end='go to next line\n')
+print("at new line")
+print(a, b, 16,end='go to next line')
+print("at new line")
