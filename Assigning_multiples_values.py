9d02d03f2d8519ba1dc30c428a2b94b442cbfb81
Rizwan Haider <rizuu004@gmail.com>
2023-12-05 19:24:53 +0500

    Add files via upload
    Multiple values assigning program

---
+++ Assigning_multiples_values.py
@@ -0,0 +1,13 @@
+name, Fname, dob, reg = "Rizwan Haider", "Haider", "12-Jan-2005", "SP22-BCS-198"
+print("Name is                 :  ",name)
+print("Father name is          :  ",Fname)
+print("Date of birth is        :  ",dob)
+print("Registration Number is  :  ",reg)
+
+
+a , b, c, d, e = 1, 2, 3, 4, 5
+print(a, b, c, d, e)
+print(type(a))
+b = str(a)
+print(b)
+print(type(b))
