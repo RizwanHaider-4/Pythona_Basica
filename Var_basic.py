b54faa180c5bf5bce5f18940f0f7568ef0a712d1
Rizwan Haider <rizuu004@gmail.com>
2023-12-05 18:59:44 +0500

    Add files via upload
    Cases of Varaible naming

---
+++ Var_basic.py
@@ -0,0 +1,23 @@
+# Three Cases
+
+# 1 
+# Camel Case
+# first words small case
+# remaining in words first letter in upper case
+
+myVariableName = "Hello"
+
+# 2
+# Pascal case
+# each words starts with capital letter
+
+MyVariableName = "Hello"
+
+
+# 3
+# Snake case
+# each words in small case
+# separated by underscore
+
+my_varaiable_name = "Hello"
+
