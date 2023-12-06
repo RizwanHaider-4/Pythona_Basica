d96a71b2e162584f4c2bbf7fb3d73d2b6fabe657
Rizwan Haider <rizuu004@gmail.com>
2023-12-06 19:41:33 +0500

    Add files via upload
    more practice on type casting

---
+++ Type_Casting 2.py
@@ -0,0 +1,23 @@
+a = " "
+b = bool(a)
+print("1.  ",b)
+
+a = ""
+b = bool(a)
+print("2.  ",b)
+
+a = True
+b = False
+c = int(a)
+d = int(b)
+print("3.   ",a,b)
+print("4.   ",c,d)
+
+a = "Riz"
+b = bool(a)
+print("5.   ",a)
+print("5.   ",b)
+
+c = 65
+b = c.char()
+print(b)
