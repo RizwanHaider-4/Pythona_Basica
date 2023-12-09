3dfca8dc9e6ec0ec9bf25cd6fffb7af1d756afb1
Rizwan Haider <rizuu004@gmail.com>
2023-12-09 15:55:49 +0500

    Add files via upload
    Identifiers derails

---
+++ 4. identifiers.py
@@ -0,0 +1,20 @@
+# identify some thing
+# 1 variable
+# 2 Symbolic constant
+# 3 name of function
+# 4 class name
+
+# symbolic constant
+
+# whoose value does not change throughout the program
+# written in upper case-letter
+# underscore in between or starting or at the end
+
+MAX_VALUE = 100
+i = int(input("Enter a number of Students:    "))
+
+if i>MAX_VALUE:
+    print("Number of Student is not more than", MAX_VALUE)
+else:
+    print("Enter the records of Students")
+    
